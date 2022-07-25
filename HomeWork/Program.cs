6004b4e0166bb48a51a1e93f0312c04d5d9a53e4
Ekaterina <kat_izh@mail.ru>
2022-07-25 22:20:07 +0300

    ShowArray

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -10,3 +10,13 @@
 
     return array;
 }
+
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+        Console.WriteLine();
+}
+

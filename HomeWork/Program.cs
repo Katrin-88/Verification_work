7a0f4ec933661561786d9477780825373515b657
Ekaterina <kat_izh@mail.ru>
2022-07-25 22:13:21 +0300

    CreateStringArray

---
+++ HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] CreateStringArray(int size)
+{
+    string[] array = new string[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i+1} word: ");
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}

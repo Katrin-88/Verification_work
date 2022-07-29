936b51eee976a5b5a6b5242fc01290d7ad31da5e
Ekaterina <kat_izh@mail.ru>
2022-07-29 16:29:30 +0300

    ThreeValueArray done

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -7,35 +7,33 @@
         Console.Write($"Input {i+1} word: ");
         array[i] = Console.ReadLine();
     }
-
     return array;
 }
 
-
-void ShowArray(string[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-
-        Console.WriteLine();
-}
-
-
-int NumberOfWords(string[] array)
+int CountOfWords(string[] array)
 {
     int count = 0;
     int length = array.Length;
     for(int i = 0; i < array.Length; i++)
+    {
         if(array[i].Length <= 3)
-            count++;
-
+        count++;
+    }
     return count;
 }
 
-Console.Write("Input number of names: ");
-int size = Convert.ToInt32(Console.ReadLine());
-
-string[] array = CreateStringArray(size);
-
-Console.WriteLine("Number of word is " + NumberOfWords(array));
-
+string[] ThreeValueArray(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        int length = array[i].Length;
+        if(length <= 3)
+        {
+             newArray[j] = array[i];
+             j++;
+        }
+    }
+    return newArray;
+}
